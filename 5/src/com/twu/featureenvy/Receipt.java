4e174bba0d824ad874547b47559776dc962a0177
Peng Ma <peng.ma@thoughtworks.com>
2022-12-04 16:52:35 +0800

    [refactor][5] extract member variable

--- 5/src/com/twu/featureenvy/Receipt.java
+++ 5/src/com/twu/featureenvy/Receipt.java
@@ -2,16 +2,19 @@ package com.twu.featureenvy;
 
 public class Receipt {
     private final Taxi taxi;
+    private double totalCost = 0d;
+    private double fixedCharge = 50d;
 
     public Receipt(Taxi taxi) {
         this.taxi = taxi;
     }
 
-    public double totalCost() {
-        double totalCost = 0;
+    public Receipt(Taxi taxi, double fixedCharge) {
+        this.taxi = taxi;
+        this.fixedCharge = fixedCharge;
+    }
 
-        // fixed charges
-        totalCost += 50;
+    public double totalCost() {
 
         // taxi charges
         int totalKms = taxi.getTotalKms();
@@ -19,9 +22,11 @@ public class Receipt {
         if(taxi.isAirConditioned()) {
             totalCost += Math.min(10, totalKms) * 20 * peakTimeMultiple;
             totalCost += Math.max(0, totalKms - 10) * 17 * peakTimeMultiple;
+            totalCost += fixedCharge;
         } else {
             totalCost += Math.min(10, totalKms) * 15 * peakTimeMultiple;
             totalCost += Math.max(0, totalKms - 10) * 12 * peakTimeMultiple;
+            totalCost += fixedCharge;
         }
 
         return totalCost * (1 + 0.1);
