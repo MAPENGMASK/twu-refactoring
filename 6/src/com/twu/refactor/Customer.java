050744f83c2cdf72b9d3af6a7524136a780d69b2
Peng Ma <peng.ma@thoughtworks.com>
2022-12-13 23:20:33 +0800

    [refactor][6] extract method

--- 6/src/com/twu/refactor/Customer.java
+++ 6/src/com/twu/refactor/Customer.java
@@ -8,75 +8,89 @@ import static com.twu.refactor.MovieTypeEnum.NEW_RELEASE;
 
 public class Customer {
 
-	private String name;
-	private List<Rental> rentalList = new ArrayList<>();
+    private final String name;
+    private final List<Rental> rentalList = new ArrayList<>();
 
-	public Customer(String name) {
-		this.name = name;
-	}
+    public Customer(String name) {
+        this.name = name;
+    }
 
-	public void addRental(Rental arg) {
-		rentalList.add(arg);
-	}
+    public void addRental(Rental rental) {
+        rentalList.add(rental);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Iterator<Rental> rentals = rentalList.iterator();
-		StringBuilder result = new StringBuilder();
-		result.append("Rental Record for ");
-		result.append(getName());
-		result.append("\n");
-		while (rentals.hasNext()) {
-			double thisAmount = 0;
-			Rental each = rentals.next();
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator<Rental> rentals = rentalList.iterator();
+        StringBuilder result = new StringBuilder();
+        printHeader(result);
+        while (rentals.hasNext()) {
+            Rental next = rentals.next();
+            double thisAmount = calculateOneRentalAmount(next);
+            frequentRenterPoints += getFrequentRenterPoints(next);
+            totalAmount += thisAmount;
+            printContent(result, thisAmount, next);
+        }
+        printFooter(totalAmount, frequentRenterPoints, result);
+        return result.toString();
+    }
 
-			// determine amounts for each line
-			switch (each.getMovie().getTypeEnum()) {
-			case REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
+    private int getFrequentRenterPoints(Rental each) {
+        int frequentRenterPoint = 0;
+        if ((NEW_RELEASE.equals(each.getMovie().getTypeEnum()))
+                && each.getDaysRented() > 1) {
+            return frequentRenterPoint + 2;
+        } else {
+            return frequentRenterPoint + 1;
+        }
+    }
 
-			}
+    private double calculateOneRentalAmount(Rental rental) {
+        double thisAmount = 0;
+        switch (rental.getMovie().getTypeEnum()) {
+            case REGULAR -> {
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2) {
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                }
+            }
+            case NEW_RELEASE -> thisAmount += rental.getDaysRented() * 3;
+            case CHILDRENS -> {
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3) {
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                }
+            }
+        }
+        return thisAmount;
+    }
 
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((NEW_RELEASE.equals(each.getMovie().getTypeEnum()))
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+    private void printFooter(double totalAmount, int frequentRenterPoints, StringBuilder result) {
+        result.append("Amount owed is ");
+        result.append(totalAmount);
+        result.append("\n");
+        result.append("You earned ");
+        result.append(frequentRenterPoints);
+        result.append(" frequent renter points");
+    }
 
-			// show figures for this rental
-			result.append("\t");
-			result.append(each.getMovie().getTitle());
-			result.append("\t");
-			result.append(thisAmount);
-			result.append("\n");
-			totalAmount += thisAmount;
+    private void printContent(StringBuilder result, double thisAmount, Rental rental) {
+        result.append("\t");
+        result.append(rental.getMovie().getTitle());
+        result.append("\t");
+        result.append(thisAmount);
+        result.append("\n");
+    }
 
-		}
-		// add footer lines
-		result.append("Amount owed is ");
-		result.append(totalAmount);
-		result.append("\n");
-		result.append("You earned ");
-		result.append(frequentRenterPoints);
-		result.append(" frequent renter points");
-		return result.toString();
-	}
+    private void printHeader(StringBuilder result) {
+        result.append("Rental Record for ");
+        result.append(this.name);
+        result.append("\n");
+    }
 
 }
